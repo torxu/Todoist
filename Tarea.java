135dd03bfaca5bbc345d45790db8cfba83e371bd
torxu <aitoroterocarpintero@gmail.com>
2016-12-15 10:56:18 +0100

    Añadidas las clases ListaTareas y Tarea y añadirTarea

---
+++ Tarea.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Tarea here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tarea
+{
+    private String descripcion;
+    
+    public Tarea()
+    {
+        descripcion = "";
+    }
+    
+    public void descripcionTarea(String nuevaTarea)
+    {
+        descripcion = nuevaTarea;
+    }
+}
